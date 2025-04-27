752491f51bb576a1dc463d6dd93b2eb803acf921
LuyenNDTSU-PTON-JV241226 <dinhluyen2402@gmail.com>
2025-04-28 00:46:20 +0900

    client

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import TodoForm from "./components/TodoForm";
-import "./App.css";
+import './App.css'
+import TodoForm from './components/TodoForm'
 
 function App() {
-    return (
-        <div className="h-[100vh] w-full flex items-center justify-center font-[Inter]">
-            <TodoForm></TodoForm>
-        </div>
-    );
+
+
+  return (
+    <>
+    <div className="h-[100vh] w-full flex items-center justify-center font-[Inter]">
+      <TodoForm></TodoForm>
+    </div>
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -4,14 +4,23 @@ import AddTodo from "./AddTodo";
 import TodoList from "./TodoList";
 import FooterActions from "./FooterActions";
 import axios from "axios";
+import { override } from "../common/loading";
+import MoonLoader from "react-spinners/MoonLoader";
 
 const TodoForm = () => {
+    const [loading, setLoading] = useState(true);
     const [listData, setListData] = useState([]);
 
     const handleGetListData = async () => {
-        axios.get(`http://localhost:3000/listTodo`).then((res) => {
+        try {
+            setLoading(true);
+            const res = await axios.get(`http://localhost:9999/listTodo`);
             setListData(res.data);
-        });
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
     };
 
     useEffect(() => {
@@ -19,12 +28,22 @@ const TodoForm = () => {
     }, []);
 
     return (
-        <div className="min-h-screen bg-gray-100 flex items-center justify-center w-full">
+        <div className="min-h-screen bg-gray-50 flex items-center justify-center w-full">
+            {loading && (
+                <div className="absolute inset-0 flex justify-center items-center z-50 rounded-lg opacity-65 bg-gray-700">
+                    <MoonLoader
+                        loading={loading}
+                        size={50}
+                        cssOverride={override}
+                        color="#ffffff"
+                    />
+                </div>
+            )}
             <div className="max-w-md w-full p-6 bg-white rounded-lg shadow">
                 <h1 className="text-center text-2xl font-semibold mb-4">
                     Quản lý công việc
                 </h1>
-                <AddTodo />
+                <AddTodo/>
                 <TodoFilter />
                 <TodoList todos={listData} />
                 <FooterActions />
